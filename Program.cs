8fde261cf6a9251f89521cd1e93340a43e2190c9
1746262 <1746262@GY350X18>
2023-12-04 16:15:28 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Text_Based_Adventure_2
+{
+    internal class Program
+    {
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Name = " ";
+        }
+    }
+}
